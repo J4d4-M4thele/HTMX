5542f0029ce2d9736ad2d6b75f4715350a51602d
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-07-30 16:49:26 +0200

    update

--- server.js
+++ server.js
@@ -4,9 +4,23 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
+
+app.get("/users", (req, res) => {
+    const users = [
+        { id: 1, name: "Jane Doe" },
+        { id: 2, name: "Gabby Halie" },
+        { id: 3, name: "Matilda Botha" }
+    ];
+    res.send(`
+        <h2>Users</h2>
+        <ul class="list-group">
+            ${users.map((user) => `<li class="list-group-item">${user.name}</li>`).join('')}
+        </ul>
+    `);
+});
 
 app.listen(3000, () => {
     console.log("Server is running on port: 3000");
