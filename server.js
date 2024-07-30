101758c9d24f4ab6957f044bb80721242d40d052
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-07-30 17:18:00 +0200

    update

--- server.js
+++ server.js
@@ -8,12 +8,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
-app.get("/users", (req, res) => {
-    const users = [
-        { id: 1, name: "Jane Doe" },
-        { id: 2, name: "Gabby Halie" },
-        { id: 3, name: "Matilda Botha" }
-    ];
+app.get("/users", async (req, res) => {
+    const limit = +req.query.limit || 10;
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users?_limit=${limit}`);
+    const users = await response.json();
     res.send(`
         <h2>Users</h2>
         <ul class="list-group">
