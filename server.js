06cee4b06b84efd952e09ec0006caf711c72d7ec
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-07-31 17:34:54 +0200

    update

--- server.js
+++ server.js
@@ -20,6 +20,15 @@ app.get("/users", async (req, res) => {
     `);
 });
 
+app.post("/calculate", (req, res) => {
+    const height = parseFloat(req.body.height);
+    const weight = parseFloat(req.body.weight);
+    const bmi = weight/(height * height);
+    res.send(`
+        <p>Height of ${height}m and Weight of ${weight}kg gives a BMI of ${bmi.toFixed(2)}kg/m²</p>
+    `);
+});
+
 app.listen(3000, () => {
     console.log("Server is running on port: 3000");
 });
