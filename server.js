8a73b023efd454596b38c1e2ac8cb45f85520266
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-08-01 15:20:33 +0200

    update

--- server.js
+++ server.js
@@ -36,6 +36,33 @@ app.get("/get-price", (req, res) => {
     res.send(`$${currentPrice.toFixed(1)}`);
 });
 
+app.post("/search", async (req, res) => {
+    const searchTerm = req.body.search.toLowerCase();
+
+    if(!searchTerm) {
+        return res.send(
+            "<tr></tr>"
+        );
+    }
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+    const users = await response.json();
+
+    const searchResults = users.filter((user) => {
+        const name = user.name.toLowerCase();
+        const email = user.email.toLowerCase();
+        return name.includes(searchTerm) || email.includes(searchTerm)
+    })
+
+    const searchResultHTML = searchResults.map((user) => {
+        `<tr>
+            <td>${user.name}</td>
+            <td>${user.email}</td>
+        </tr>`
+    }).join('');
+
+    res.send(searchResultHTML)
+});
+
 app.listen(3000, () => {
     console.log("Server is running on port: 3000");
 });
