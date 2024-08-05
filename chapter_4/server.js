b2061bd8e3424180755d1d3295414eebe4e7f05c
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-08-05 09:31:46 +0200

    update

--- chapter_4/server.js
+++ chapter_4/server.js
@@ -8,34 +8,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
-app.get("/users", async (req, res) => {
-    const limit = +req.query.limit || 10;
-    const response = await fetch(`https://jsonplaceholder.typicode.com/users?_limit=${limit}`);
-    const users = await response.json();
-    res.send(`
-        <h2>Users</h2>
-        <ul class="list-group">
-            ${users.map((user) => `<li class="list-group-item">${user.name}</li>`).join('')}
-        </ul>
-    `);
-});
-
-app.post("/calculate", (req, res) => {
-    const height = parseFloat(req.body.height);
-    const weight = parseFloat(req.body.weight);
-    const bmi = weight/(height * height);
-    res.send(`
-        <p>Height of ${height}m and Weight of ${weight}kg gives a BMI of ${bmi.toFixed(2)}kg/m²</p>
-    `);
-});
-
-let currentPrice = 60;
-
-app.get("/get-price", (req, res) => {
-    currentPrice = currentPrice + Math.random() * 2 - 1;
-    res.send(`$${currentPrice.toFixed(1)}`);
-});
-
 app.post("/search", async (req, res) => {
     const searchTerm = req.body.search.toLowerCase();
 
@@ -60,7 +32,7 @@ app.post("/search", async (req, res) => {
         </tr>`
     }).join('');
 
-    res.send(searchResultHTML)
+    res.send(searchResultHTML);
 });
 
 app.listen(3000, () => {
